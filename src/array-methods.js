4a71e564a50f7a0917c5b73f90bca7f6ea7ba802
Kalan Prudhomme <cleaver9@gmail.com>
2021-05-06 20:14:13 -0700

    test setup

---
+++ src/array-methods.js
@@ -0,0 +1,61 @@
+export function howMuchPencil(str) {
+
+}
+
+export function wordsToCharList(arr) {
+
+}
+
+const gruffaloCrumble = {
+  name: 'How to make a Gruffalo Crumble',
+  ingredients: [
+    '1 medium-sized Gruffalo',
+    '8 pounds oats',
+    '2 pounds brown sugar',
+    '4 pounds flour',
+    '2 gallons pure maple syrup',
+    '16 cups chopped nuts',
+    '1 pound baking soda',
+    '1 pound baking powder',
+    '1 pound cinnamon',
+    '6 gallons melted butter',
+    '2 gallons fresh water',
+  ],
+  steps: [
+    'Pre-heat a large oven to 375',
+    'De-prickle the gruffalo',
+    'Sprinkle with cinnamon, sugar, flour, and nuts',
+    'Mix until evenly distributed',
+    'Grease a 3-foot x 3-foot casserole dish',
+    'Combine gruffalo compote with water to maintain moisture in the oven',
+    'Fold together remaining ingredients to make the crisp',
+    'Spread the crisp evenly over the gruffalo mixture',
+    'Bake for 12-15 hours',
+  ]
+}; 
+
+export function listFoods(recipe) {
+
+}
+
+export function stepActions(recipe) {
+
+}
+
+export function removeLastCharacters(str, numberOfCharacters) {
+
+}
+
+//stretch goals
+
+export function totalSumCSV(str) {
+
+}
+
+export function removeVowels(str) {
+
+}
+
+export function extractVowels(str) {
+  
+}
---
+++ src/array-methods.test.js
@@ -0,0 +1,36 @@
+import { howMuchPencil, wordsToCharList, listFoods, stepActions, removeLastCharacters, totalSumCSV, removeVowels, extractVowels } from './array-methods';
+
+test('It should return a list of shortening words', () => {
+  expect(howMuchPencil('Welcome')).toStrictEqual(['Welcome', 'elcome', 'lcome', 'come', 'ome', 'me', 'e', '']);
+  expect(howMuchPencil('Welcome').length).toStrictEqual(8);
+  expect(howMuchPencil('')).toStrictEqual(['']);
+  expect(howMuchPencil('abc')).toStrictEqual(['abc', 'bc', 'c', '']);
+});
+
+
+test('It should return a set of characters', () => {
+  expect(wordsToCharList('Gregor')).toStrictEqual(['G', 'r', 'e', 'g', 'o', 'r']);
+  expect(wordsToCharList('Gregor').length).toStrictEqual(6);
+  expect(wordsToCharList('hooray')).toStrictEqual(['h', 'o', 'o', 'r', 'a', 'y']);
+  expect(wordsToCharList('')).toStrictEqual([]);
+});
+
+
+test('It should return a list of items', () => {
+  expect(listFoods('gruffaloCrumble')).toStrictEqual(['Gruffalo', 'oats', 'brown sugar', 'flour', 'pure maple syrup', 'chopped nuts', 'baking soda', 'baking powder', 'cinnamon', 'melted butter', 'fresh water']);
+    
+});
+
+test('It should return step actions', () => {
+  expect(listFoods('gruffaloCrumble')).toStrictEqual(['Pre-heat', 'De-prickle', 'Sprinkle', 'Mix', 'Grease', 'Combine', 'Fold', 'Spread', 'Bake']);
+    
+});
+
+test('It should remove last characters', () => {
+  expect(removeLastCharacters('Gregor', 2)).toStrictEqual('Greg');
+  expect(removeLastCharacters('hello', -1)).toStrictEqual('hello');
+  expect(removeLastCharacters('wowow', -700)).toStrictEqual('wowow');
+  expect(removeLastCharacters('hello', 12)).toStrictEqual('');
+  expect(removeLastCharacters('', 1)).toStrictEqual('');
+  expect(removeLastCharacters('a', 1)).toStrictEqual('');
+});
