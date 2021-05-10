a00dbdac9a9505c1878bcd1f68e38456c3d353ad
Kalan Prudhomme <cleaver9@gmail.com>
2021-05-10 15:33:30 -0700

    test setup

---
+++ src/for-each.js
@@ -0,0 +1,19 @@
+export function returnTen(str) {
+
+}
+
+export function findMax(matrix) {
+
+}
+
+export function totalSum(matrix) {
+
+}
+
+export function grandTotal(stores) {
+
+}
+
+export function salesData(hours, data) {
+  
+}
