6d4b9121e2d49434cf6c91c568dc0ef28ad2e7b6
Kalan Prudhomme <cleaver9@gmail.com>
2021-05-14 13:48:17 -0700

    test setup

--- src/code-challenge-13.test.js
+++ src/code-challenge-13.test.js
@@ -46,14 +46,14 @@ let characters = [
 ];
 
 describe('Testing challenge 1', () => {
-  test('It should sort the characters by number of children', () => {
+  test.skip('It should sort the characters by number of children', () => {
     expect(sortByChildren(characters)[0].name).toStrictEqual('Euron');
     expect(sortByChildren(characters)[0].children.length).toStrictEqual(0);
   });
 });
 
-describe('Testing challenge 2', () => {
-  test('It should return true if it has a W', () => {
+describe('.skiping challenge 2', () => {
+  test.skip('It should return true if it has a W', () => {
     expect(containsW('hello world')).toStrictEqual(true);
     expect(containsW('Hello World')).toStrictEqual(false);
     expect(containsW('hello everyone')).toStrictEqual(false);
@@ -61,7 +61,7 @@ describe('Testing challenge 2', () => {
 });
 
 describe('Testing challenge 3', () => {
-  test('It should return true if it has a number', () => {
+  test.skip('It should return true if it has a number', () => {
     expect(isNum(1234567890)).toStrictEqual(true);
     expect(isNum('12345')).toStrictEqual(true);
     expect(isNum('h3llo w0rld')).toStrictEqual(true);
@@ -71,7 +71,7 @@ describe('Testing challenge 3', () => {
 });
 
 describe('Testing challenge 4', () => {
-  test('It should return true if it has world, case sensitive', () => {
+  test.skip('It should return true if it has world, case sensitive', () => {
     expect(containsWorld('hello world')).toStrictEqual(true);
     expect(containsWorld('Hello World')).toStrictEqual(false);
     expect(containsWorld('hello everyone')).toStrictEqual(false);
@@ -79,7 +79,7 @@ describe('Testing challenge 4', () => {
 });
 
 describe('Testing challenge 5', () => {
-  test('It should return words if they contain a capital letter', () => {
+  test.skip('It should return words if they contain a capital letter', () => {
     expect(isCapitalized('We only want to Return the Words that begin With a capital Letter')).toStrictEqual(['We', 'Return', 'Words', 'With', 'Letter']);
     expect(isCapitalized('Given by our hand in the meadow that is called Runnymede, between Windsor and Staines, on the fifteenth day of June in the seventeenth year of our reign (i.e. 1215: the new regnal year began on 28 May).')).toStrictEqual(['Given', 'Runnymede', 'Windsor', 'Staines', 'June', 'May']);
     expect(isCapitalized('these words are all failures')).toStrictEqual([]);
@@ -87,7 +87,7 @@ describe('Testing challenge 5', () => {
 });
 
 describe('Testing challenge 6', () => {
-  test('It should return cities starting with letters a to j', () => {
+  test.skip('It should return cities starting with letters a to j', () => {
     expect(citiesAtoJ(['Cleveland', 'San Diego', 'Birmingham', 'Seattle', 'Miami', 'New York City', 'Omaha', 'Portland', 'Austin', 'Boston', 'Newport Beach', 'Hoboken'])).toStrictEqual(['Cleveland', 'Birmingham', 'Austin', 'Boston', 'Hoboken']);
     expect(citiesAtoJ(['Albuquerque', 'Chicago', 'Philadelphia', 'Newark', 'Sacramento', 'Eugene'])).toStrictEqual(['Albuquerque', 'Chicago', 'Eugene']);
     expect(citiesAtoJ([])).toStrictEqual([]);
---
+++ src/code-challenge-14.js
@@ -0,0 +1,23 @@
+export const replaceZeros = (string) => {
+
+};
+
+export const validatePin = (pin) => {
+
+};
+
+export const validateWord = (word) => {
+
+};
+
+export const hasNumber = (string) => {
+
+};
+
+export const validateEmail = (email) => {
+
+};
+
+export const validatePhoneNumber = (phoneNumber) => {
+
+};
